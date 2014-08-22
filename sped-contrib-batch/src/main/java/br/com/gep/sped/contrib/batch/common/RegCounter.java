fd196bbda910cd10d0ca062640c4e67c8e9dc68d
Elvis Luciano <elvis.gs@gmail.com>
2014-08-22 10:10:51 -0300

    Usando job scope no regCounter e regIdHolder

--- sped-contrib-batch/src/main/java/br/com/gep/sped/contrib/batch/common/RegCounter.java
+++ sped-contrib-batch/src/main/java/br/com/gep/sped/contrib/batch/common/RegCounter.java
@@ -1,11 +1,13 @@
 package br.com.gep.sped.contrib.batch.common;
 
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@JobScope
 public class RegCounter {
 
     private final Map<Class, Integer> counts;
--- sped-contrib-batch/src/main/java/br/com/gep/sped/contrib/batch/common/RegIdHolder.java
+++ sped-contrib-batch/src/main/java/br/com/gep/sped/contrib/batch/common/RegIdHolder.java
@@ -1,5 +1,6 @@
 package br.com.gep.sped.contrib.batch.common;
 
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -9,13 +10,14 @@ import java.util.Map;
  * Armazena o último id de cada registro lido
  */
 @Component
+@JobScope
 public class RegIdHolder {
 
     private Map<Class<?>, Integer> ids = new HashMap<Class<?>, Integer>();
 
     /**
      * Obtém último id do registro representado pela classe
-     * @param regClass Classe que representa o registro
+     * @param regClass Classe do registro
      * @return Id do registro
      */
     public Integer getId(Class<?> regClass) {
@@ -27,7 +29,7 @@ public class RegIdHolder {
 
     /**
      * Seta id do registro representado pela classe
-     * @param regClass Classe que representa o registro
+     * @param regClass Classe do registro
      * @param id Id a ser armazenado
      */
     public void setId(Class<?> regClass, Integer id) {
