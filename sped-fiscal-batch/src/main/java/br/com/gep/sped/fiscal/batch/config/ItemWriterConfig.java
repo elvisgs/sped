774eb94eebb529910b72089fc8bc8f6b93d33da9
Elvis Luciano <elvis.gs@gmail.com>
2014-10-17 12:09:06 -0300

    Implementação de ItemWriterFactory que cria writer beanIO

---
+++ sped-fiscal-batch/src/main/java/br/com/gep/sped/fiscal/batch/config/ItemWriterConfig.java
@@ -0,0 +1,40 @@
+package br.com.gep.sped.fiscal.batch.config;
+
+import br.com.gep.sped.fiscal.marshaller.writer.SpedFiscalWriterFactory;
+import br.com.gep.sped.marshaller.common.Registro;
+import br.com.gep.sped.marshaller.common.writer.SpedWriterFactory;
+import org.beanio.spring.BeanIOFlatFileItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.File;
+
+@Configuration
+public class ItemWriterConfig {
+
+    private static final String OVERRIDDEN_BY_EXPRESSION = null;
+
+    @Bean
+    @StepScope
+    public <T extends Registro> BeanIOFlatFileItemWriter<T> beanIOWriter(@Value("#{jobParameters['output.file.name']}") String outputFileName) {
+        BeanIOFlatFileItemWriter<T> writer = new BeanIOFlatFileItemWriter<>();
+        SpedWriterFactory factory = SpedFiscalWriterFactory.getInstance();
+        writer.setStreamFactory(factory.getStreamFactory());
+        writer.setStreamName(factory.getStreamName());
+        writer.setTransactional(false);
+        writer.setAppendAllowed(true);
+        writer.setSaveState(false);
+
+        File file = new File(outputFileName);
+        writer.setResource(new FileSystemResource(file));
+
+        return writer;
+    }
+
+    public <T extends Registro> BeanIOFlatFileItemWriter<T> beanIOWriter() {
+        return beanIOWriter(OVERRIDDEN_BY_EXPRESSION);
+    }
+}
---
+++ sped-fiscal-batch/src/main/java/br/com/gep/sped/fiscal/batch/factory/BeanIOItemWriterFactory.java
@@ -0,0 +1,25 @@
+package br.com.gep.sped.fiscal.batch.factory;
+
+import br.com.gep.sped.batch.common.factory.ItemWriterFactory;
+import br.com.gep.sped.fiscal.batch.config.ItemWriterConfig;
+import br.com.gep.sped.marshaller.common.Registro;
+import org.springframework.batch.item.ItemStreamWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanIOItemWriterFactory implements ItemWriterFactory {
+
+    @Autowired
+    private ItemWriterConfig itemWriterConfig;
+
+    @Override
+    public <R extends Registro, P extends Registro> ItemStreamWriter<R> create(Class<R> regClass, Class<P> parentRegClass) {
+        return itemWriterConfig.beanIOWriter();
+    }
+
+    @Override
+    public <R extends Registro> ItemStreamWriter<R> create(Class<R> regClass) {
+        return create(regClass, null);
+    }
+}
