0f6902b2db4a122616441d8bf692397c78171048
Elvis Luciano <elvis.gs@gmail.com>
2014-10-17 11:30:20 -0300

    Correção e refatoração da árvore do sped contrib

--- sped-contrib-batch/src/main/java/br/com/gep/sped/contrib/batch/util/SpedContribTree.java
+++ sped-contrib-batch/src/main/java/br/com/gep/sped/contrib/batch/util/SpedContribTree.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Component;
 
 /**
  * Representa a árvore de registros do SPED Contribuições.
- * Obs: Registros *010 não são considerados filhos do registro *001 do bloco.
  */
 @Component
 public class SpedContribTree extends SpedTree {
@@ -39,15 +38,15 @@ public class SpedContribTree extends SpedTree {
     }
 
     private void addBranchesBloco0() {
-        nodes.put(Reg0000.class, new RegNode(Reg0000.class));
-        nodes.put(Reg0001.class, new RegNode(Reg0001.class));
-        nodes.put(Reg0035.class, new RegNode(Reg0035.class));
-        nodes.put(Reg0100.class, new RegNode(Reg0100.class));
-        nodes.put(Reg0110.class, new RegNode(Reg0110.class).addChildren(
+        addNode(new RegNode(Reg0000.class));
+        addNode(new RegNode(Reg0001.class, Reg0000.class));
+        addNode(new RegNode(Reg0035.class, Reg0001.class));
+        addNode(new RegNode(Reg0100.class, Reg0001.class));
+        addNode(new RegNode(Reg0110.class, Reg0001.class).addChildren(
                 new RegNode(Reg0111.class)
         ));
-        nodes.put(Reg0120.class, new RegNode(Reg0120.class));
-        nodes.put(Reg0140.class, new RegNode(Reg0140.class).addChildren(
+        addNode(new RegNode(Reg0120.class, Reg0001.class));
+        addNode(new RegNode(Reg0140.class, Reg0001.class).addChildren(
                 new RegNode(Reg0145.class),
                 new RegNode(Reg0150.class),
                 new RegNode(Reg0190.class),
@@ -59,14 +58,14 @@ public class SpedContribTree extends SpedTree {
                 new RegNode(Reg0400.class),
                 new RegNode(Reg0450.class)
         ));
-        nodes.put(Reg0500.class, new RegNode(Reg0500.class));
-        nodes.put(Reg0600.class, new RegNode(Reg0600.class));
-        nodes.put(Reg0990.class, new RegNode(Reg0990.class));
+        addNode(new RegNode(Reg0500.class, Reg0001.class));
+        addNode(new RegNode(Reg0600.class, Reg0001.class));
+        addNode(new RegNode(Reg0990.class));
     }
 
     private void addBranchesBlocoA() {
-        nodes.put(RegA001.class, new RegNode(RegA001.class));
-        nodes.put(RegA010.class, new RegNode(RegA010.class).addChildren(
+        addNode(new RegNode(RegA001.class, Reg0000.class));
+        addNode(new RegNode(RegA010.class, RegA001.class).addChildren(
                 new RegNode(RegA100.class).addChildren(
                         new RegNode(RegA110.class),
                         new RegNode(RegA111.class),
@@ -74,12 +73,12 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegA170.class)
                 )
         ));
-        nodes.put(RegA990.class, new RegNode(RegA990.class));
+        addNode(new RegNode(RegA990.class));
     }
 
     private void addBranchesBlocoC() {
-        nodes.put(RegC001.class, new RegNode(RegC001.class));
-        nodes.put(RegC010.class, new RegNode(RegC010.class).addChildren(
+        addNode(new RegNode(RegC001.class, Reg0000.class));
+        addNode(new RegNode(RegC010.class, RegC001.class).addChildren(
                 new RegNode(RegC100.class).addChildren(
                         new RegNode(RegC110.class),
                         new RegNode(RegC111.class),
@@ -128,12 +127,12 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegC609.class)
                 )
         ));
-        nodes.put(RegC990.class, new RegNode(RegC990.class));
+        addNode(new RegNode(RegC990.class));
     }
 
     private void addBranchesBlocoD() {
-        nodes.put(RegD001.class, new RegNode(RegD001.class));
-        nodes.put(RegD010.class, new RegNode(RegD010.class).addChildren(
+        addNode(new RegNode(RegD001.class, Reg0000.class));
+        addNode(new RegNode(RegD010.class, RegD001.class).addChildren(
                 new RegNode(RegD100.class).addChildren(
                         new RegNode(RegD101.class),
                         new RegNode(RegD105.class),
@@ -161,12 +160,12 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegD609.class)
                 )
         ));
-        nodes.put(RegD990.class, new RegNode(RegD990.class));
+        addNode(new RegNode(RegD990.class));
     }
 
     private void addBranchesBlocoF() {
-        nodes.put(RegF001.class, new RegNode(RegF001.class));
-        nodes.put(RegF010.class, new RegNode(RegF010.class).addChildren(
+        addNode(new RegNode(RegF001.class, Reg0000.class));
+        addNode(new RegNode(RegF010.class, RegF001.class).addChildren(
                 new RegNode(RegF100.class).addChildren(
                         new RegNode(RegF111.class)
                 ),
@@ -183,29 +182,29 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegF211.class)
                 ),
                 // TODO: esses registros dependem da versão do SPED e estão relacionados ao registro 0110
-//                new RegNode(RegF500.class).addChildren(
-//                        new RegNode(RegF509.class)
-//                ),
-//                new RegNode(RegF510.class).addChildren(
-//                        new RegNode(RegF519.class)
-//                ),
-//                new RegNode(RegF525.class),
-//                new RegNode(RegF550.class).addChildren(
-//                        new RegNode(RegF559.class)
-//                ),
-//                new RegNode(RegF560.class).addChildren(
-//                        new RegNode(RegF569.class)
-//                ),
+                /*new RegNode(RegF500.class).addChildren(
+                        new RegNode(RegF509.class)
+                ),
+                new RegNode(RegF510.class).addChildren(
+                        new RegNode(RegF519.class)
+                ),
+                new RegNode(RegF525.class),
+                new RegNode(RegF550.class).addChildren(
+                        new RegNode(RegF559.class)
+                ),
+                new RegNode(RegF560.class).addChildren(
+                        new RegNode(RegF569.class)
+                ),*/
                 new RegNode(RegF600.class),
                 new RegNode(RegF700.class),
                 new RegNode(RegF800.class)
         ));
-        nodes.put(RegF990.class, new RegNode(RegF990.class));
+        addNode(new RegNode(RegF990.class));
     }
 
     private void addBranchesBlocoI() {
-        nodes.put(RegI001.class, new RegNode(RegI001.class));
-        nodes.put(RegI010.class, new RegNode(RegI010.class).addChildren(
+        addNode(new RegNode(RegI001.class, Reg0000.class));
+        addNode(new RegNode(RegI010.class, RegI001.class).addChildren(
                 new RegNode(RegI100.class).addChildren(
                         new RegNode(RegI200.class).addChildren(
                                 new RegNode(RegI300.class).addChildren(
@@ -216,16 +215,16 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegI199.class)
                 )
         ));
-        nodes.put(RegI990.class, new RegNode(RegI990.class));
+        addNode(new RegNode(RegI990.class));
     }
 
     private void addBranchesBlocoM() {
-        nodes.put(RegM001.class, new RegNode(RegM001.class));
-        nodes.put(RegM100.class, new RegNode(RegM100.class).addChildren(
+        addNode(new RegNode(RegM001.class, Reg0000.class));
+        addNode(new RegNode(RegM100.class, RegM001.class).addChildren(
                 new RegNode(RegM105.class),
                 new RegNode(RegM110.class)
         ));
-        nodes.put(RegM200.class, new RegNode(RegM200.class).addChildren(
+        addNode(new RegNode(RegM200.class, RegM001.class).addChildren(
                 // TODO: esse registro depende da versão do SPED
                 //new RegNode(RegM205.class),
                 new RegNode(RegM210.class).addChildren(
@@ -234,16 +233,16 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegM230.class)
                 )
         ));
-        nodes.put(RegM300.class, new RegNode(RegM300.class));
-        nodes.put(RegM350.class, new RegNode(RegM350.class));
-        nodes.put(RegM400.class, new RegNode(RegM400.class).addChildren(
+        addNode(new RegNode(RegM300.class, RegM001.class));
+        addNode(new RegNode(RegM350.class, RegM001.class));
+        addNode(new RegNode(RegM400.class, RegM001.class).addChildren(
                 new RegNode(RegM410.class)
         ));
-        nodes.put(RegM500.class, new RegNode(RegM500.class).addChildren(
+        addNode(new RegNode(RegM500.class, RegM001.class).addChildren(
                 new RegNode(RegM505.class),
                 new RegNode(RegM510.class)
         ));
-        nodes.put(RegM600.class, new RegNode(RegM600.class).addChildren(
+        addNode(new RegNode(RegM600.class, RegM001.class).addChildren(
                 // TODO: esse registro depende da versão do SPED
                 //new RegNode(RegM605.class),
                 new RegNode(RegM610.class).addChildren(
@@ -252,44 +251,44 @@ public class SpedContribTree extends SpedTree {
                         new RegNode(RegM630.class)
                 )
         ));
-        nodes.put(RegM700.class, new RegNode(RegM700.class));
-        nodes.put(RegM800.class, new RegNode(RegM800.class).addChildren(
+        addNode(new RegNode(RegM700.class, RegM001.class));
+        addNode(new RegNode(RegM800.class, RegM001.class).addChildren(
                 new RegNode(RegM810.class)
         ));
-        nodes.put(RegM990.class, new RegNode(RegM990.class));
+        addNode(new RegNode(RegM990.class));
     }
 
     private void addBranchesBlocoP() {
-        nodes.put(RegP001.class, new RegNode(RegP001.class));
-        nodes.put(RegP010.class, new RegNode(RegP010.class).addChildren(
+        addNode(new RegNode(RegP001.class, Reg0000.class));
+        addNode(new RegNode(RegP010.class, RegP001.class).addChildren(
                 new RegNode(RegP100.class).addChildren(
                         new RegNode(RegP110.class),
                         new RegNode(RegP199.class)
                 )
         ));
-        nodes.put(RegP200.class, new RegNode(RegP200.class).addChildren(
+        addNode(new RegNode(RegP200.class, RegP001.class).addChildren(
                 new RegNode(RegP210.class)
         ));
-        nodes.put(RegP990.class, new RegNode(RegP990.class));
+        addNode(new RegNode(RegP990.class));
     }
 
     private void addBranchesBloco1() {
-        nodes.put(Reg1001.class, new RegNode(Reg1001.class));
-        nodes.put(Reg1010.class, new RegNode(Reg1010.class));
-        nodes.put(Reg1020.class, new RegNode(Reg1020.class));
-        nodes.put(Reg1100.class, new RegNode(Reg1100.class));
-        nodes.put(Reg1300.class, new RegNode(Reg1300.class));
-        nodes.put(Reg1500.class, new RegNode(Reg1500.class));
-        nodes.put(Reg1700.class, new RegNode(Reg1700.class));
-        nodes.put(Reg1800.class, new RegNode(Reg1800.class));
-        nodes.put(Reg1900.class, new RegNode(Reg1900.class));
-        nodes.put(Reg1990.class, new RegNode(Reg1990.class));
+        addNode(new RegNode(Reg1001.class, Reg0000.class));
+        addNode(new RegNode(Reg1010.class, Reg1001.class));
+        addNode(new RegNode(Reg1020.class, Reg1001.class));
+        addNode(new RegNode(Reg1100.class, Reg1001.class));
+        addNode(new RegNode(Reg1300.class, Reg1001.class));
+        addNode(new RegNode(Reg1500.class, Reg1001.class));
+        addNode(new RegNode(Reg1700.class, Reg1001.class));
+        addNode(new RegNode(Reg1800.class, Reg1001.class));
+        addNode(new RegNode(Reg1900.class));
+        addNode(new RegNode(Reg1990.class));
     }
 
     private void addBranchesBloco9() {
-        nodes.put(Reg9001.class, new RegNode(Reg9001.class));
-        nodes.put(Reg9900.class, new RegNode(Reg9900.class));
-        nodes.put(Reg9990.class, new RegNode(Reg9990.class));
-        nodes.put(Reg9999.class, new RegNode(Reg9999.class));
+        addNode(new RegNode(Reg9001.class, Reg0000.class));
+        addNode(new RegNode(Reg9900.class, Reg9001.class));
+        addNode(new RegNode(Reg9990.class));
+        addNode(new RegNode(Reg9999.class));
     }
 }
