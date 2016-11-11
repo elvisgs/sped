700a0614d5b640e89067d4f87c39fb6ace355f26
Elvis Luciano <elvis.gs@gmail.com>
2016-11-11 21:06:42 -0200

    Unificação de classes de configuração

--- sped-fiscal-batch/src/main/java/br/com/gep/sped/fiscal/batch/factory/BeanIOItemWriterFactory.java
+++ sped-fiscal-batch/src/main/java/br/com/gep/sped/fiscal/batch/factory/BeanIOItemWriterFactory.java
@@ -1,26 +1,47 @@
 package br.com.gep.sped.fiscal.batch.factory;
 
+import br.com.gep.sped.batch.common.factory.BeanIOFlatFileItemWriterFactoryBean;
 import br.com.gep.sped.batch.common.factory.ItemWriterFactory;
-import br.com.gep.sped.fiscal.batch.config.ItemWriterConfig;
+import br.com.gep.sped.fiscal.marshaller.writer.SpedFiscalWriterFactory;
 import br.com.gep.sped.marshaller.common.Registro;
+import org.beanio.spring.BeanIOFlatFileItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemStreamWriter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+import java.io.File;
+
+import static br.com.gep.sped.batch.common.SpedJobParameterBuilder.OUTPUT_FILE_NAME_EL;
+
+@Configuration
 public class BeanIOItemWriterFactory implements ItemWriterFactory {
 
-    @Autowired
-    private ItemWriterConfig itemWriterConfig;
+    private static final String OVERRIDDEN_BY_EXPRESSION = null;
 
     @SuppressWarnings("unchecked")
     @Override
     public <R extends Registro, P extends Registro> ItemStreamWriter<R> create(Class<R> regClass, Class<P> parentRegClass) {
-        return itemWriterConfig.beanIOWriter();
+        return beanIOWriter();
     }
 
     @Override
     public <R extends Registro> ItemStreamWriter<R> create(Class<R> regClass) {
         return create(regClass, null);
+    }
+
+    @Bean
+    @StepScope
+    @Value(OUTPUT_FILE_NAME_EL)
+    BeanIOFlatFileItemWriter beanIOWriter(String outputFileName) {
+        return new BeanIOFlatFileItemWriterFactoryBean()
+            .setSpedWriterFactory(SpedFiscalWriterFactory.getInstance())
+            .setOutputFile(new File(outputFileName))
+            .getObject();
+    }
+
+    BeanIOFlatFileItemWriter beanIOWriter() {
+        return beanIOWriter(OVERRIDDEN_BY_EXPRESSION);
     }
 }
