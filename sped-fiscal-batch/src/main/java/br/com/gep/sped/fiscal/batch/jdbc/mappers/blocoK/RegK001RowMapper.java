1d4c2412e3f81b2f301f8a3cf1fdf96bc65088ed
Elvis Luciano <elvis.gs@gmail.com>
2017-04-15 02:06:36 -0300

    Correção: faltando coluna ind_mov no rowMapper do registro K001

--- sped-fiscal-batch/src/main/java/br/com/gep/sped/fiscal/batch/jdbc/mappers/blocoK/RegK001RowMapper.java
+++ sped-fiscal-batch/src/main/java/br/com/gep/sped/fiscal/batch/jdbc/mappers/blocoK/RegK001RowMapper.java
@@ -12,6 +12,7 @@ public class RegK001RowMapper implements RowMapper<RegK001> {
     public RegK001 mapRow(ResultSet rs, int rowNum) throws SQLException {
         RegK001 reg = new RegK001();
         reg.setId(rs.getInt("id"));
+        reg.setIndMov(rs.getString("ind_mov"));
         return reg;
     }
 }
